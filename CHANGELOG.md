7feb33c8d76ff5657d13fd4c7590468c8a639794
Afolabi Ayanfe <ayanfesolutions@gmail.com>
2024-08-13 11:03:31 +0100

    publish version 1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ## 1.0.0
 
 * Full implementation of fincra checkout
+
+## 1.1.0
+
+* Fix payment page not loading
--- test/fincra_payment_test.dart
+++ test/fincra_payment_test.dart
@@ -1,7 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:fincra_payment/fincra_payment.dart';
-
-void main() {
- 
-}
+void main() {}
